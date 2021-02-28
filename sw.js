10485c19b99c3c4ef228b0435949a4dd8fe1cdf7
allania7med11 <allania7med11@gmail.com>
2021-02-28 14:32:51 +0100

    test

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const options = {"workboxURL":"https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.4/workbox/workbox-sw.js","importScripts":[],"config":{"debug":false},"cacheOptions":{"cacheId":"frontend_tt-prod","directoryIndex":"/","revision":"HQi4xrfYNm15"},"clientsClaim":true,"skipWaiting":true,"cleanupOutdatedCaches":true,"offlineAnalytics":false,"preCaching":[{"revision":"HQi4xrfYNm15","url":"/?standalone=true"}],"runtimeCaching":[{"urlPattern":"/_nuxt/","handler":"CacheFirst","method":"GET","strategyPlugins":[]},{"urlPattern":"/","handler":"NetworkFirst","method":"GET","strategyPlugins":[]}],"offlinePage":null,"pagesURLPattern":"/","offlineStrategy":"NetworkFirst"}
-
+// test
 importScripts(...[options.workboxURL, ...options.importScripts])
 
 initWorkbox(workbox, options)
